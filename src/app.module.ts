66b4e3e437b43da5d8cd721fc21134d05135c62e
chisom promise <chimisepro@gmail.com>
2023-08-12 03:38:13 +0100

    feat: register database

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './users/user.module';
+import { MessageModule } from './messages/message.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { validate, loader} from './common/config';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    envFilePath: `.env.${process.env.NODE_ENV}`,
+    validate,
+    load: [loader]
+  }), TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
+      const isProduction = configService.get('env') === 'production';
+      return {
+        ssl: isProduction,
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+        host: configService.get('db.host'),
+        port: configService.get('db.port'),
+        username: configService.get('db.username'),
+        password: configService.get('db.password'),
+        database: configService.get('db.database')
+      }
+    }
+  }), UserModule, MessageModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
